1d4313c1960fb33ea07a3927a65a7fce61985a41
st166379 <st166379@stud.uni-stuttgart.de>
2024-04-17 22:32:40 +0200

    Directions

--- Assignment 1/PeriodicSensorReading/app/src/main/java/com/unistuttgart/periodicsensorreading/CircleWithLineView.kt
+++ Assignment 1/PeriodicSensorReading/app/src/main/java/com/unistuttgart/periodicsensorreading/CircleWithLineView.kt
@@ -11,11 +11,14 @@ import kotlin.math.sin
 class CircleWithLineView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
-        strokeWidth = 4f
+        strokeWidth = 5f
+        textSize = 100f
     }
     private val center = PointF()
     private var radius = 0f
     private var angle = 0f
+    private var width = 0f
+    private var height = 0f
 
     fun setAngle(angle: Float) {
         this.angle = (angle - 90) % 360
@@ -25,12 +28,20 @@ class CircleWithLineView(context: Context, attrs: AttributeSet) : View(context, 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         center.set(w / 2f, h / 2f)
-        radius = Math.min(w, h) / 2f
+        radius = Math.min(w, h) / 2f - 5
+        width = w.toFloat()
+        height = h.toFloat()
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawCircle(center.x, center.y, radius, paint)
+
+        canvas.drawText("N", width/2f - 35f, 100f, paint)
+        canvas.drawText("S", width/2f - 35f, height - 20f, paint)
+        canvas.drawText("W", 20f, height/2f + 35f, paint)
+        canvas.drawText("E", width - 90f, height/2f + 35f, paint)
+
         val endX = center.x + radius * cos(Math.toRadians(angle.toDouble())).toFloat()
         val endY = center.y + radius * sin(Math.toRadians(angle.toDouble())).toFloat()
         canvas.drawLine(center.x, center.y, endX, endY, paint)
