12b4519ae651374f206bb1924753520b215b3736
Alexander Riedlinger <st167220@stud.uni-stuttgart.de>
2024-04-30 15:40:53 +0200

    Liste der Geräte geht, nur das Verbinden via GATT geht nicht

--- Assignment 2/WeatherApp/app/src/main/java/com/unistuttgart/weatherapp/BluetoothScanner.kt
+++ Assignment 2/WeatherApp/app/src/main/java/com/unistuttgart/weatherapp/BluetoothScanner.kt
@@ -2,6 +2,9 @@ package com.unistuttgart.weatherapp
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter.LeScanCallback
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
@@ -13,28 +16,13 @@ import com.unistuttgart.weatherapp.databinding.ActivityMainBinding
 @SuppressLint("MissingPermission")
 class BluetoothScanner (private var context: Context, binding: ActivityMainBinding) {
     private var bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-
     private var bluetoothScanner: BluetoothLeScanner? = bluetoothManager.adapter.bluetoothLeScanner
 
-    //private var callback: LeScanCallback = LeScanCallback { device, rssi, scanRecord ->
-
-        // Do something with the device and the RSSI
-    //}
-
-    private var callback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: android.bluetooth.le.ScanResult) {
-            // Do something with the device and the RSSI
-            val address = result.device.address
-            val specificUuid = ParcelUuid.fromString("00000002-0000-0000-FDFD-FDFDFDFDFDFD")
-
-            if (address == "F6:B6:2A:79:7B:5D") {
-                Toast.makeText(context, "Found the device™", Toast.LENGTH_SHORT).show()
-            }
-        }
+    private var gattCallback: BluetoothGattCallback = object : BluetoothGattCallback() {
     }
 
 
-    fun startScan() {
+    fun startScan(callback:ScanCallback) {
         bluetoothScanner?.startScan(callback)
     }
 
--- Assignment 2/WeatherApp/app/src/main/java/com/unistuttgart/weatherapp/MainActivity.kt
+++ Assignment 2/WeatherApp/app/src/main/java/com/unistuttgart/weatherapp/MainActivity.kt
@@ -9,7 +9,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.unistuttgart.weatherapp.databinding.ActivityMainBinding
 import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.le.ScanCallback
 import android.os.Build
+import android.os.ParcelUuid
+import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
@@ -18,6 +25,49 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val bluetoothPermissionRequestCode = 69420
     private lateinit var bluetoothScanner: BluetoothScanner
+    private var devices = mutableSetOf<BluetoothDevice>()
+    private var weatherGatt: BluetoothGatt? = null
+    private var fanGatt: BluetoothGatt? = null
+
+    private var callback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: android.bluetooth.le.ScanResult) {
+            // Do something with the device and the RSSI
+            /**
+            val address = result.device.address
+            val specificUuid = ParcelUuid.fromString("00000002-0000-0000-FDFD-FDFDFDFDFDFD")
+
+            if (address == "F6:B6:2A:79:7B:5D") {
+                Toast.makeText(this@MainActivity, "Found the device", Toast.LENGTH_SHORT).show()
+            }*/
+            val device = result.device
+            if (!devices.contains(device)) {
+                devices.add(device)
+                addDeviceButton(device)
+            }
+        }
+    }
+
+    private var weatherGattCallback:BluetoothGattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            super.onConnectionStateChange(gatt, status, newState)
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                Toast.makeText(this@MainActivity, "Connected to Weather Station", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Disconnected from Weather Station", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private var fanGattCallback:BluetoothGattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            super.onConnectionStateChange(gatt, status, newState)
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                Toast.makeText(this@MainActivity, "Connected to Fan", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Disconnected from Fan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +81,41 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        checkBluetoothPermissions()
+
         bluetoothScanner = BluetoothScanner(this, binding)
-        bluetoothScanner.startScan()
+        bluetoothScanner.startScan(callback)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun addDeviceButton(device: BluetoothDevice) {
+        val button = Button(this)
+        button.text = device.name ?: device.address
+        when (device.address) {
+            "F6:B6:2A:79:7B:5D" -> {
+                button.text = "Weather Station"
+                button.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+                button.setOnClickListener {
+                    val gatt = device.connectGatt(this, false, weatherGattCallback)
+                    // Speichern Sie das BluetoothGatt-Objekt für zukünftige Interaktionen
+                    weatherGatt = gatt
+                }
+            }
+            "F8:20:74:F7:2B:82" -> {
+                button.text = "Fan"
+                button.setBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+                button.setOnClickListener {
+                    val gatt = device.connectGatt(this, false, fanGattCallback)
+                    // Speichern Sie das BluetoothGatt-Objekt für zukünftige Interaktionen
+                    fanGatt = gatt
+                }
+            }
+            else -> {
+                button.setOnClickListener {
+                    Toast.makeText(this, "Ich mach garrrnix.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        binding.deviceContainer.addView(button)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
