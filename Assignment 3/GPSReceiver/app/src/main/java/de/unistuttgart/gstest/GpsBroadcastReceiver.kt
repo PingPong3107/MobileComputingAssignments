ada3d28afb3c1c0d3191c6a415a88c05027e6080
Alexander Riedlinger <st167220@stud.uni-stuttgart.de>
2024-05-17 12:35:26 +0200

    aufgabe 2 umbenannt und übersetzt

---
+++ Assignment 3/GPSReceiver/app/src/main/java/de/unistuttgart/gstest/GpsBroadcastReceiver.kt
@@ -0,0 +1,65 @@
+package de.unistuttgart.gstest
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.os.Environment
+import android.util.Log
+import de.unistuttgart.gstest.gpsservice.MyLocationListener
+import me.himanshusoni.gpxparser.GPXWriter
+import me.himanshusoni.gpxparser.modal.GPX
+import me.himanshusoni.gpxparser.modal.Track
+import me.himanshusoni.gpxparser.modal.TrackSegment
+import me.himanshusoni.gpxparser.modal.Waypoint
+import java.io.File
+import java.io.FileOutputStream
+import java.util.Date
+
+
+class GpsBroadcastReceiver: BroadcastReceiver() {
+    private val gpxWriter= GPXWriter()
+    private val gpxFile = GPX()
+    private lateinit var track : Track
+    private lateinit var trackSegment: TrackSegment
+    var waypointList = mutableListOf<Waypoint>()
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent != null) {
+            when (intent.action) {
+
+                MyLocationListener.LOCATION_CHANGED -> locationChanged(
+                    intent.getParcelableExtra("location")!!
+                )
+                MyLocationListener.SERVICE_STARTSTOP -> {
+                    val status = intent.getIntExtra("status", -1)
+                    if (status == 1) {
+                        Log.i("GpsBroadcastReceiver", "Service started")
+                        trackSegment = TrackSegment()
+                        track = Track()
+                        gpxFile.addTrack(track)
+                        track.addTrackSegment(trackSegment)
+                    } else if (status == 0) {
+                        Log.i("GpsBroadcastReceiver", "Service stopped")
+                        saveGPX()
+                    }
+                }
+            }
+        }
+    }
+    private fun locationChanged(location: Location) {
+        Log.i("GpsBroadcastReceiver", "Location changed: $location")
+        val waypoint = Waypoint(location.latitude, location.longitude)
+        val date = Date(location.time)
+        waypoint.time = date
+        trackSegment.addWaypoint(waypoint)
+        waypointList.add(waypoint)
+    }
+
+    private fun saveGPX(){
+        val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "outFile.gpx")
+        val out = FileOutputStream(file)
+        gpxWriter.writeGPX(gpxFile,out)
+        out.close()
+    }
+}
---
+++ Assignment 3/GPSReceiver/app/src/main/java/de/unistuttgart/gstest/gpsservice/MyLocationManager.kt
@@ -0,0 +1,41 @@
+package de.unistuttgart.gstest.gpsservice
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.provider.Settings
+import androidx.core.content.ContextCompat
+
+class MyLocationManager(private val context: Context) {
+    private var locationManager: LocationManager? = null
+    private val locationListener: MyLocationListener
+
+    init {
+        locationManager = ContextCompat.getSystemService(context, LocationManager::class.java)
+        locationListener = MyLocationListener(context)
+        enableGps()
+    }
+
+    private fun isGpsEnabled(): Boolean {
+        return locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER) ?: false
+    }
+
+    private fun enableGps() {
+        if (!isGpsEnabled()) {
+            val enableGpsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            context.startActivity(enableGpsIntent)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun requestLocationUpdates() {
+        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1f, locationListener)
+
+    }
+
+    fun removeLocationUpdates() {
+        locationManager?.removeUpdates(locationListener)
+    }
+
+}
