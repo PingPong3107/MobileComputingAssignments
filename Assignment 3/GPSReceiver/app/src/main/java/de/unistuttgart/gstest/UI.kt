7f9393c3d2a1343c8d09632ac2823e03b029eedb
Leon <st166379@stud.uni-stuttgart.de>
2024-05-21 14:38:49 +0200

    Update-Knopf nur bei laufendem Service sichtbar

--- Assignment 3/GPSReceiver/app/src/main/java/de/unistuttgart/gstest/UI.kt
+++ Assignment 3/GPSReceiver/app/src/main/java/de/unistuttgart/gstest/UI.kt
@@ -4,11 +4,13 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import de.unistuttgart.gstest.gpsservice.GpsBackgroundService
 
 class UI (private val activity: Activity, private val context: Context) {
     private val startButton: Button = activity.findViewById(R.id.startService)
+    private val updateButton: Button = activity.findViewById(R.id.updateValues)
     private val serviceIntent = Intent(activity, GpsBackgroundService::class.java)
     private var isRunning = false
 
@@ -17,11 +19,13 @@ class UI (private val activity: Activity, private val context: Context) {
             if(!isRunning){
                 activity.startService(serviceIntent)
                 startButton.text = context.getString(R.string.stop_button_text)
+                updateButton.visibility = View.VISIBLE
                 isRunning = true
             }
             else{
                 activity.stopService(serviceIntent)
                 startButton.text = context.getString(R.string.start_service)
+                updateButton.visibility = View.INVISIBLE
                 isRunning = false
             }
         }
