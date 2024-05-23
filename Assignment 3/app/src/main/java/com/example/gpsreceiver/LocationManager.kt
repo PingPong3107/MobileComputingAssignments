e5656a32faed4b28f575164a268ae6ccba4196cd
Joel Waimer <st167572@stud.uni-stuttgart.de>
2024-05-23 10:22:16 +0200

    Graphiken für die Eddystone-App

---
+++ Assignment 3/app/src/main/java/com/example/gpsreceiver/LocationManager.kt
@@ -0,0 +1,12 @@
+package com.example.gpsreceiver
+import android.location.LocationManager
+import android.location.LocationListener
+import android.location.Location
+
+class LocationCallback : LocationListener {
+
+    @Override
+    public override fun onLocationChanged(location: Location){
+
+    }
+}
---
+++ Assignment 3/app/src/main/java/com/example/gpsreceiver/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.gpsreceiver
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.subscribe)
+        button.setOnClickListener({
+            
+        })
+    }
+
+
+}
