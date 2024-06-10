7ad999af6eef33af625f456bf3b153f0217fbc00
Ingo Schwendinger <st166046@stud.uni-stuttgart.de>
2024-06-10 15:00:58 +0200

    so netzt müsste es aber gehen mit dem shell skript

--- Assignment 4/execute.sh
+++ Assignment 4/execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # IP-Adressen oder Hostnamen der Raspberry Pis
-HOSTS=("129.69.210.60" "129.69.210.61" "129.69.210.62" "129.69.210.63" "129.69.210.64" "129.69.210.65" "129.69.210.66")
+HOSTS=("129.69.210.61" "129.69.210.63" "129.69.210.64" "129.69.210.65" "129.69.210.66")
 
 # Benutzername für die Raspberry Pis
 USERNAME="team6"
@@ -10,14 +10,18 @@ USERNAME="team6"
 PASSWORD="ohn2IDef"
 
 # Pfad zum Skript, das auf den Raspberry Pis ausgeführt werden soll
-REMOTE_SCRIPT="/home/team6/test.py"
-
+REMOTE_SCRIPT="/home/team6/receiver.py"
 
 open_terminal() {
     HOST=$1
     konsole --hold -e bash -c "
-        sshpass -p '$PASSWORD' ssh -t $USERNAME@$HOST <<'EOF'
-        python3 $REMOTE_SCRIPT
+        sshpass -p '$PASSWORD' ssh -t $USERNAME@$HOST <<EOF
+        echo 'Verwende fuser, um den Prozess auf Port 5006 zu beenden...'
+        fuser -k 5006/udp || true
+        echo 'Prozess beendet oder kein Prozess auf dem Port.'
+        echo 'Starte das Python-Skript...'
+        PYTHONUNBUFFERED=1 python3 $REMOTE_SCRIPT
+        echo 'Python-Skript gestartet.'
         exec bash
 EOF
     "
@@ -30,4 +34,3 @@ for HOST in "${HOSTS[@]}"; do
 done
 
 echo "Alle Konsolen wurden geöffnet."
-
