3cedfcd6391c8abe70ca13d6edf0536fd3996930
Joel Waimer <st167572@stud.uni-stuttgart.de>
2024-06-11 16:29:44 +0200

    Zwischenbeitrag; bei Fragen zum DSR-Knoten gerne mir schreiben

--- Assignment 4/dsr.py
+++ Assignment 4/dsr.py
@@ -3,11 +3,21 @@ from utils import generate_message, MessageTypes
 
 
 def discover_route(destination: str, routing_table:dict):
+    #Create and broadcast a route request message
     discovery_message = generate_message(destination, MessageTypes.ROUTE_REQUEST, f"Route to {destination}")
+    send_message(discovery_message)
+
+"""
+Use this method to send a message to the destination via a pre-determined route
+"""
+def send_routed(destination: str, message_payload: str, routing_table: dict):
+    if destination not in routing_table.keys():
+        discover_route(destination, routing_table)
     
 
-def send_routed(destination: str, message: str):
-
-
 if __name__ == "__main__":
+    #Generate a dicitionary, which maps an IP-address (type: String) to a neighbouring node
     routes: dict = {}
+    #If this node has been determined to be the source node in this network, send a message to the specified destination
+    if(sys.argv[2] == "quell"):
+        send_routed(sys.argv[1], "Verbum Domini manet in aeternum!")
