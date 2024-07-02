e86c71d707d04a9b0ae6ac255eca6d687ee6316c
Ingo Schwendinger <st166046@stud.uni-stuttgart.de>
2024-07-02 13:46:03 +0200

    read and write geht schon mal basically

--- Assignment 5/FollUebleDatabase/app/src/main/java/de/uni_s/ipvs/mcl/assignment5/MainActivity.kt
+++ Assignment 5/FollUebleDatabase/app/src/main/java/de/uni_s/ipvs/mcl/assignment5/MainActivity.kt
@@ -1,14 +1,28 @@
 package de.uni_s.ipvs.mcl.assignment5
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private lateinit var database:FirebaseDatabase
+    private lateinit var team6ref: DatabaseReference
+    private lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,16 +32,68 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        listView = findViewById(R.id.listview)
+        database = Firebase.database
+        team6ref = database.getReference("teams").child("6")
+        insertTemperatureData("Stuttgart","2011-2-2", System.currentTimeMillis().toString(), 25.0)
+        fetchCities()
 
-        val database = Firebase.database
-        val myRef = database.getReference("teams").child("6")
 
-        // Read from the database
-        myRef.get().addOnSuccessListener {
+    }
+
+    private fun insertTemperatureData(city: String, date: String, time: String, temperature: Double) {
+        team6ref.child("location").child(city).child(date).child(time).setValue(temperature)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    println("Temperaturdaten für $city am $date um $time erfolgreich eingefügt: $temperature")
+                } else {
+                    println("Fehler beim Einfügen der Temperaturdaten: ${task.exception}")
+                }
+            }
+    }
+
+    private fun readTemperatureData(city: String, date: String, time: String) {
+        team6ref.child("location").child(city).child(date).child(time).get().addOnSuccessListener {
             val value = it.value
-            println("Value is: $value")
+            println("Temperaturdaten für $city am $date um $time: $value")
         }.addOnFailureListener {
-            println("Error getting data: ${it.message}")
+            println("Fehler beim Lesen der Temperaturdaten: ${it.message}")
         }
     }
+
+
+    private fun deleteTestingStuff(){
+        team6ref.child("location").removeValue().addOnCompleteListener {
+            println("Alle Daten gelöscht")
+        }
+    }
+
+    private fun fetchCities() {
+        val teamId = "6"  // Example team ID
+        val citiesRef = team6ref.child("location")
+
+        citiesRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val cityList = mutableListOf<String>()
+
+                for (citySnapshot in dataSnapshot.children) {
+                    val cityName = citySnapshot.key
+                    cityName?.let { cityList.add(it) }
+                }
+
+                // Display the cities in the ListView
+                val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, cityList)
+                listView.adapter = adapter
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("MainActivity", "Database error: ${databaseError.message}")
+            }
+        })
+    }
+
+
 }
+
+
+
